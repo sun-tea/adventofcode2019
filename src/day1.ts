4de4f43982461cfd75f773c68207b2f97df88f49
Xuan-thi Nguyen <xthi.ngu@gmail.com>
2019-12-01 12:42:47 +0100

    day 1 part 2

--- src/day1.ts
+++ src/day1.ts
@@ -1,12 +1,26 @@
 // Day 1: The Tyranny of the Rocket Equation
 import { parseFile } from 'helpers';
 
-const part1 = content =>
-  content.reduce((acc, mass) => acc + Math.floor(mass / 3) - 2, 0);
+const getRequiredFuel = (acc, mass) => acc + Math.floor(mass / 3) - 2;
+
+const part1 = content => content.reduce(getRequiredFuel, 0);
+
+const getRequiredFuelRecursive = mass => {
+  const requiredFuel = Math.floor(mass / 3) - 2;
+
+  if (requiredFuel <= 0) {
+    return 0;
+  }
+  return requiredFuel + getRequiredFuelRecursive(requiredFuel);
+};
+
+const part2 = content =>
+  content.reduce((acc, mass) => acc + getRequiredFuelRecursive(mass), 0);
 
 (() => {
   const args = process.argv.slice(2);
   const filePath = args[0] || 'day1.txt';
   const content = parseFile(filePath);
   console.log(`part1: ${part1(content)}`);
+  console.log(`part2: ${part2(content)}`);
 })();
