ed19d2c0350c4ebfddb38c07b623e9ec9c43fa5f
Xuan-thi Nguyen <xthi.ngu@gmail.com>
2019-12-15 19:16:52 +0100

    day 4 part 1

---
+++ src/day4.ts
@@ -0,0 +1,20 @@
+// Day 4: Secure Container
+
+const part1 = content => {
+  const [start, end] = content.split('-');
+  const possiblePasswords = [];
+
+  const firstRegex = /(\d)\1/;
+  const secondRegex = /^(?=\d{6}$)0*1*2*3*4*5*6*7*8*9*$/;
+  for (let i = start; i <= end; i++) {
+    if (firstRegex.exec(i) && secondRegex.exec(i)) {
+      possiblePasswords.push(i);
+    }
+  }
+
+  return possiblePasswords.length;
+};
+
+(() => {
+  console.log(`part1: ${part1('357253-892942')}`);
+})();
