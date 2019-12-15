acf84a74508e1e7411a3b4af3ceaeedf4790223b
Xuan-thi Nguyen <xthi.ngu@gmail.com>
2019-12-15 20:48:49 +0100

    day 4 part 2

--- src/day4.ts
+++ src/day4.ts
@@ -15,6 +15,22 @@ const part1 = content => {
   return possiblePasswords.length;
 };
 
+const part2 = content => {
+  const [start, end] = content.split('-');
+  const possiblePasswords = [];
+
+  const firstRegex = /(?:^|(.)(?!\1))(\d)\2(?!\2)/;
+  const secondRegex = /^(?=\d{6}$)0*1*2*3*4*5*6*7*8*9*$/;
+  for (let i = start; i <= end; i++) {
+    if (firstRegex.exec(i) && secondRegex.exec(i)) {
+      possiblePasswords.push(i);
+    }
+  }
+
+  return possiblePasswords.length;
+};
+
 (() => {
   console.log(`part1: ${part1('357253-892942')}`);
+  console.log(`part2: ${part2('357253-892942')}`);
 })();
