399c728aba50acc16273ea0b73973959b4630c7b
Xuan-thi Nguyen <xthi.ngu@gmail.com>
2019-12-15 19:16:40 +0100

    day 3 part 2

--- src/day3.ts
+++ src/day3.ts
@@ -8,79 +8,52 @@ const parseDirection = (value: string) => {
   return { direction, distance };
 };
 
+const getStepsScore = (path, intersection) =>
+  path.findIndex(([x, y]) => x === intersection[0] && y === intersection[1]);
+
 const checkCrossingPath = (path, position) =>
   path.some(([x, y]) => x === position[0] && y === position[1]);
 
-const up = (path, distance, crossingPoints = null, pathFirstWire = null) => {
+const move = (path, distance, direction, data) => {
   const [x, y] = path[path.length - 1];
   for (let i = 1; i <= distance; i++) {
-    const nextPos = [x, y + i];
-    if (crossingPoints && checkCrossingPath(pathFirstWire, nextPos)) {
-      crossingPoints.push(nextPos);
+    if (data && typeof data.stepsScore !== 'undefined') {
+      data.stepsScore++;
+    }
+
+    const nextPos =
+      direction === 'U'
+        ? [x, y + i]
+        : direction === 'D'
+        ? [x, y - i]
+        : direction === 'L'
+        ? [x - i, y]
+        : [x + i, y];
+    if (data && checkCrossingPath(data.pathFirstWire, nextPos)) {
+      if (typeof data.stepsScore !== 'undefined') {
+        const firstScore = getStepsScore(data.pathFirstWire, nextPos);
+        data.crossingPoints.push({
+          coords: nextPos,
+          scores: [firstScore, data.stepsScore],
+        });
+      } else {
+        data.crossingPoints.push({ coords: nextPos });
+      }
     }
     path.push(nextPos);
   }
 };
 
-const down = (path, distance, crossingPoints = null, pathFirstWire = null) => {
-  const [x, y] = path[path.length - 1];
-  for (let i = 1; i <= distance; i++) {
-    const nextPos = [x, y - i];
-    if (crossingPoints && checkCrossingPath(pathFirstWire, nextPos)) {
-      crossingPoints.push(nextPos);
-    }
-    path.push(nextPos);
-  }
+const generatePath = ({ path, data }, { direction, distance }) => {
+  move(path, distance, direction, data);
+  return { path, data };
 };
 
-const right = (path, distance, crossingPoints = null, pathFirstWire = null) => {
-  const [x, y] = path[path.length - 1];
-  for (let i = 1; i <= distance; i++) {
-    const nextPos = [x + i, y];
-    if (crossingPoints && checkCrossingPath(pathFirstWire, nextPos)) {
-      crossingPoints.push(nextPos);
-    }
-    path.push(nextPos);
-  }
-};
-
-const left = (path, distance, crossingPoints = null, pathFirstWire = null) => {
-  const [x, y] = path[path.length - 1];
-  for (let i = 1; i <= distance; i++) {
-    const nextPos = [x - i, y];
-    if (crossingPoints && checkCrossingPath(pathFirstWire, nextPos)) {
-      crossingPoints.push(nextPos);
-    }
-    path.push(nextPos);
-  }
-};
-
-const generatePath = (
-  { path, crossingPoints, pathFirstWire },
-  { direction, distance }
-) => {
-  switch (direction) {
-    case 'U':
-      up(path, distance, crossingPoints, pathFirstWire);
-      break;
-    case 'D':
-      down(path, distance, crossingPoints, pathFirstWire);
-      break;
-    case 'R':
-      right(path, distance, crossingPoints, pathFirstWire);
-      break;
-    case 'L':
-      left(path, distance, crossingPoints, pathFirstWire);
-      break;
-  }
-  return { path, crossingPoints, pathFirstWire };
-};
-
-const shortestManhattanDistance = coords =>
-  coords.reduce((res, [x, y]) => {
+const shortestManhattanDistance = intersections =>
+  intersections.reduce((res, { coords: [x, y] }) => {
     const distance = Math.abs(x) + Math.abs(y);
     return distance < res ? distance : res;
-  }, Math.abs(coords[0][0]) + Math.abs(coords[0][1]));
+  }, Math.abs(intersections[0].coords[0]) + Math.abs(intersections[0].coords[1]));
 
 const part1 = content => {
   const firstWire = content[0].split(',').map(parseDirection);
@@ -91,18 +64,39 @@ const part1 = content => {
 
   const pathSecondWire = secondWire.reduce(generatePath, {
     path: [[0, 0]],
-    crossingPoints,
-    pathFirstWire: path,
+    data: { crossingPoints, pathFirstWire: path },
   });
 
-  const result = shortestManhattanDistance(crossingPoints);
+  return shortestManhattanDistance(crossingPoints);
+};
 
-  console.log(result);
+const part2 = content => {
+  const firstWire = content[0].split(',').map(parseDirection);
+  const secondWire = content[1].split(',').map(parseDirection);
+  let crossingPoints = [];
+
+  const { path } = firstWire.reduce(generatePath, { path: [[0, 0]] });
+
+  const pathSecondWire = secondWire.reduce(generatePath, {
+    path: [[0, 0]],
+    data: { crossingPoints, pathFirstWire: path, stepsScore: 0 },
+  });
+
+  const result = crossingPoints.reduce(
+    (res, { scores: [firstScore, secondScore] }) => {
+      const scoresSum = firstScore + secondScore;
+      return scoresSum < res ? scoresSum : res;
+    },
+    crossingPoints[0].scores[0] + crossingPoints[0].scores[1]
+  );
+
+  return result;
 };
 
 (() => {
   const args = process.argv.slice(2);
   const filePath = args[0] || 'day3.txt';
   const content = parseFile(filePath);
-  console.log(`part1: ${part1(content)}`);
+  // console.log(`part1: ${part1(content)}`);
+  console.log(`part2: ${part2(content)}`);
 })();
